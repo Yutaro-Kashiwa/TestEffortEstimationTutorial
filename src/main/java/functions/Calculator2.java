5a4482c9979505000bccc4634317c5db17e723a3
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2020-05-18 20:41:29 +0900

    Change type2

--- src/main/java/functions/Calculator2.java
+++ src/main/java/functions/Calculator2.java
@@ -3,7 +3,7 @@ package functions;
 import static java.lang.StrictMath.pow;
 
 public class Calculator2 extends Calculator {
-    public void power(int x) {
+    public void power(double x) {
         if (answer==null) return;//ignore
         answer = pow(answer, x);
     }
