0884a56b88a2be17e247111c9b909ed80c2bfaa2
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2020-05-18 20:38:46 +0900

    Change type

--- src/test/java/functions/Calculator2Test.java
+++ src/test/java/functions/Calculator2Test.java
@@ -9,14 +9,14 @@ public class Calculator2Test {
     public void testPower_N001(){
         Calculator2 calc = new Calculator2();
         calc.plus(2.0);
-        calc.power(2);
+        calc.power(2.0);
         assertEquals(calc.getAnswer(), Double.valueOf(4));
     }
     @Test
     public void testPower_N002(){
         Calculator2 calc = new Calculator2();
         calc.plus(2.0);
-        calc.power(0);
+        calc.power(0.0);
         assertEquals(calc.getAnswer(), Double.valueOf(1));
     }
 }
