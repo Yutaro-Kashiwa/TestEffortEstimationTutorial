a31303472b672d0bea585b062dfb04012f476ae7
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2020-12-05 18:41:33 +0900

    prepare

--- src/main/java/functions/Calculator.java
+++ src/main/java/functions/Calculator.java
@@ -31,9 +31,11 @@ public class Calculator<T> {
         getRating();
     }
     int getRating() {
-        return 1 > 5 ? 2 : 1;
+        return moreThanFiveLateDeliveries() ? 2 : 1;
     }
-
+    boolean moreThanFiveLateDeliveries() {
+        return this.answer > 5;
+    }
 
     public void minus( Double x){
         if(this.isNull(x, answer)){
