1fdb4b073a041eed892b971b4de0da1c7407d29c
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2020-12-25 18:47:22 +0900

    Replace Variable with Attribute

--- src/main/java/function/Calculator.java
+++ src/main/java/function/Calculator.java
@@ -22,7 +22,7 @@ public class Calculator<T> {
 
     public void plus (Double p){
         int val=0;
-        val += p.intValue();
+        this.val += p.intValue();
         System.out.println(tmpVal);
         add(p);
     }
