eefa87f06bf9ee8185105d8b7e77dd9b6dd9665a
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2020-12-25 15:28:59 +0900

    prepare for move and inline

--- src/main/java/functions/Calculator.java
+++ src/main/java/functions/Calculator.java
@@ -1,6 +1,5 @@
 package functions;
 
-import functions.special.B;
 import static java.lang.StrictMath.abs;
 
 import java.util.List;
@@ -36,7 +35,15 @@ public class Calculator<T> {
             return;
         }
         this.answer+=x;
+        getRating();
     }
+    int getRating() {
+        return moreThanFiveLateDeliveries() ? 2 : 1;
+    }
+    boolean moreThanFiveLateDeliveries() {
+        return 1 > 5;
+    }
+
 
     public void minus( Double x){
         if(this.isNull(x, answer)){
@@ -62,22 +69,6 @@ public class Calculator<T> {
         }
         this.answer/=x;
     }
-    public void aaa1(int i){}
-    public void aaa1_1(int i, double j, Double d){}
-    public void aaa2(double i){}
-    public void aaa3(float i){}
-    public void aaa4(boolean i){}
-    public void aaa5(short i){}
-    public void aaa6(long i){}
-    public void aaa7(byte i){}
-    public void aaa8(char i){}
-    public void aaa9(byte[] i){}
-    public void aaa10(Integer[] i){}
-    public void aaa11(Integer... i){}
-    public void aaa12(Double[][] i){
-        SubClass b = new SubClass();
-    }
-
 
 
 
@@ -100,35 +91,8 @@ public class Calculator<T> {
         }
         return false;
     }
-    private void aaa(Double x,
-                     Double answer,
-                     Double answer1,
-                     Double answe4) {
-        System.out.println(x+""+answer+""+answer1+""+answe4);
-    }
-    public Integer Layer1(Integer a){
-        long ii = B.i;
-        long iii = functions.special.B.i;
-        Integer j = B.Layer2_RENAME(a);
-        return j;
-    }
-    public Integer Layer1_2(List<Integer> i){
-        Integer j = B.Layer2_2(i);
-        return j;
-    }
-    public static void main(String[] args){
-        Calculator calc = new Calculator();
-        calc.plus(1.0);
-        System.out.println(calc.getAnswer());
-        String a = "/Users/yutarokashiwa/.m2/repository/org/opentest4j/opentest4j/1.1.1/opentest4j-1.1.1.jar,org/opentest4j/AssertionFailedError,org/opentest4j/AssertionFailedError,Normal,5c926c86eeaa81c12cd59061b6a0d03afd5a2f21";
-        if(a.contains("/.m2/")){
-            System.out.println("YES");
-        }
-    }
-    public static class A {
-        public static B b;
 
-    }
+
     public class SubClass{
         public SubClass(){
             ABC a = new ABC();
