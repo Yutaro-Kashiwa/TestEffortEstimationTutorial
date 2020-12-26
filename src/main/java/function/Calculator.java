b3b94db76ccebdf5705101ec5cc803ca8784f9b0
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2020-12-26 15:04:32 +0900

    prepare

--- src/main/java/function/Calculator.java
+++ src/main/java/function/Calculator.java
@@ -6,25 +6,33 @@ import static java.lang.StrictMath.pow;
 
 public class Calculator<T> {
     protected Double answer = 0.0;;
-    State s = new State();
-
+    protected int plusVal = 0;
     public Calculator(){
     }
 
-    public void power(Double x) {
-        if (answer==null) return;//ignore
-        answer = pow(answer, x);
-    }
 
     public void plus (Double p){
-        add(p);
+        add(p+plusVal);
     }
 
     protected void add(Double x) {
         if(isNull(x, answer)){
             return;
         }
-        this.answer+=x+s.getA();
+        this.answer+=x;
+    }
+
+    public Double getAnswer()
+    {
+        return answer;
+    }
+
+
+    boolean isNull(Double x, Double answer) {
+        if (x==null|answer==null){
+            return true;
+        }
+        return false;
     }
 
     public void minus( Double x){
@@ -35,57 +43,15 @@ public class Calculator<T> {
     }
 
     public void times ( Double x){
-        if(isStaticNull(x, answer)){
-            return;
-        }
         this.answer*=x;
     }
     public void divided(Double x  ){
-        if(Calculator.isStaticNull(x, answer)){
-            return;
-        }
         if (abs(x)<=0.0){
             System.err.println("the number was divided by 0");
             answer = null;
             return;
         }
         this.answer/=x;
-    }
-
-
-
-    public Double getAnswer()
-    {
-        return answer;
-    }
-    public static void aho(){
-        System.out.println(1);
-    }
-    boolean isNull(Double x, Double answer) {
-        if (x==null|answer==null){
-            return true;
-        }
-        return false;
-    }
-    public static boolean isStaticNull(Double x, Double answer) {
-        if (x==null|answer==null){
-            return true;
-        }
-        return false;
-    }
-
-
-    public class SubClass{
-        public SubClass(){
-            ABC a = new ABC();
-            SayHello();
-        }
-        public class ABC{
-
-        }
-        public void SayHello(){
-            System.out.println("Hello World");
-        }
     }
 
 }
