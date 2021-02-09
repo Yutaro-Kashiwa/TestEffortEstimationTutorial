108da56330ea456943cbbec6f6fb3f1f039e1d9b
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2021-02-09 15:11:00 +0900

    prepare

--- src/test/java/function/CalculatorTest.java
+++ src/test/java/function/CalculatorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calc;
     @Deprecated
     public static void common(){
         Integer i = 0;
@@ -13,95 +14,15 @@ class CalculatorTest {
     }
     @BeforeAll
     public void init(){
-        this is the cause of compile error;
+        calc = new Calculator();
     }
 
     @Test
     public void testAdd_N001(){
-        Calculator calc = new Calculator();
         calc.plus(1.0, 1);
         assertEquals(calc.getAnswer(), Double.valueOf(2));
         System.out.println();
         common();
     }
 
-    //    @Test
-//    public void testAdd_N002(){
-//        Calculator calc = new Calculator();
-//        calc.plus(0.0);
-//        assertEquals(calc.getAnswer(), Double.valueOf(0));
-//    }
-    @Test
-    public void testMinus_N001(){
-        Calculator calc = new Calculator();
-        calc.minus(1.0);
-        assertEquals(calc.getAnswer(), Double.valueOf(-1));
-    }
-//    @Test
-//    public void testMinus_N002(){
-//        Calculator calc = new Calculator();
-//        calc.minus(null);
-//        assertEquals(calc.getAnswer(), Double.valueOf(0));
-//    }
-//    @Test
-//    public void testTimes_N001(){
-//        Calculator calc = new Calculator();
-//        calc.times(1.0);
-//        assertEquals(calc.getAnswer(), Double.valueOf(0));
-//    }
-//    @Test
-//    public void testTimes_N002(){
-//        Calculator calc = new Calculator();
-//        calc.times(null);
-//        assertEquals(calc.getAnswer(), Double.valueOf(0));
-//    }
-//    @Test
-//    public void testDivided_N001(){
-//        Calculator calc = new Calculator();
-//        calc.divided(1.0);
-//        assertEquals(calc.getAnswer(), Double.valueOf(0));
-//    }
-//    @Test
-//    public void testDivided_N002(){
-//        Calculator calc = new Calculator();
-//        calc.divided(null);
-//        assertEquals(calc.getAnswer(), Double.valueOf(0));
-//    }
-//    @Test
-//    public void testDivided_E001(){
-//        Calculator calc = new Calculator();
-//        calc.divided(0.0);
-//        assertNull(calc.getAnswer());
-//        aho();
-//    }
-//    @Test
-//    public void testLayer_N001(){
-//        Calculator calc = new Calculator();
-//        Integer i = calc.Layer1(3);
-//        assertEquals(i, Integer.valueOf(4));
-//    }
-//    @Test
-//    public void testLayer_N002(){
-//        Calculator calc = new Calculator();
-//        List<Integer> a = new ArrayList<Integer>();
-//        a.add(1);
-//        a.add(2);
-//        Integer i = calc.Layer1_2(a);
-//        assertEquals(i, Integer.valueOf(2));
-//    }
-//
-//    public void testPower_N001(){
-//        Calculator calc = new Calculator();
-//        calc.plus(2.0);
-//        calc.power(2.0);
-//        assertEquals(calc.getAnswer(), Double.valueOf(4));
-//    }
-//    @Test
-//    public void testPower_N002(){
-//        Calculator calc = new Calculator();
-//        calc.plus(2.0);
-//        calc.power(0.0);
-//        assertEquals(calc.getAnswer(), Double.valueOf(1));
-//        Calculator.aho();
-//    }
 }
