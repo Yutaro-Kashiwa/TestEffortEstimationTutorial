cb9c5c1601292a363bcf872bb914b4f9f2d30662
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2021-09-05 23:50:08 +0900

    Rename for testing cross run

--- Calc/src/test/java/work/TestSalaryCalculation.java
+++ Calc/src/test/java/work/TestSalaryCalculation.java
@@ -1,6 +1,7 @@
 package work;
 
 import bean.Employee;
+import function.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,12 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestSalaryCalculation {
     @Test
     public void testCalc001(){
-        Employee worker1 = new Employee("Alice", 10);
-        worker1.recordWorking(3);
-        worker1.recordWorking(3);
-        worker1.recordWorking(3);
-        SalaryCalculation sc = new SalaryCalculation();
-        int ans = (int) sc.calc(worker1);
-        assertEquals(90, ans);
+        Calculator calc = new Calculator();
+        calc.devided(2);
+        System.out.println(calc.getAnswer());
     }
 }
